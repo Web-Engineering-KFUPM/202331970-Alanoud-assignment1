26b1b0f3b964fd14becfdf51d0939f97e1955dcc
pewvv <s202331970@kfupm.edu.sa>
2026-09-19 21:53:41 +0300

    JS-listen-for-click

--- js/script.js
+++ js/script.js
@@ -6,3 +6,14 @@ const savedTheme = localStorage.getItem('theme');
 if (savedTheme == 'dark') {
     body.classList.add('dark-theme');
 }
+
+//listen for the click 
+themeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-theme');
+
+    if (body.classList.contains('dark-theme')) {
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'light');
+    }
+});
